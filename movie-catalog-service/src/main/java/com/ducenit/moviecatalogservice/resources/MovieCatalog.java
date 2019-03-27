e8fad0cbced356798c52850e46aaefc43f39c2b6
Viswanathan.Mahadeva <Viswanathan.Mahadeva@DUCENTRNG-14.ducenitchn.com>
2019-03-27 14:37:46 +0530

    Completed call to movie info service.

--- movie-catalog-service/src/main/java/com/ducenit/moviecatalogservice/resources/MovieCatalog.java
+++ movie-catalog-service/src/main/java/com/ducenit/moviecatalogservice/resources/MovieCatalog.java
@@ -1,23 +1,42 @@
 package com.ducenit.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.ducenit.moviecatalogservice.models.CatalogItem;
+import com.ducenit.moviecatalogservice.models.MovieItem;
+import com.ducenit.moviecatalogservice.models.Rating;
+ 
 
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalog {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId)
 	{
-		return Collections.singletonList(new CatalogItem("Avengers","Useless",3));
+		List<Rating> movieRatings=Arrays.asList(new Rating("1",4),new Rating("2",3));
+		List<CatalogItem> movieCatalogItemList=new ArrayList<CatalogItem>();
+		CatalogItem movieCatalogItem;
+		for(int i=0;i<movieRatings.size();i++)
+		{
+			MovieItem movieItem=restTemplate.getForObject("http://localhost:8082/movies/"+movieRatings.get(i).getMovieId(),MovieItem.class);
+			movieCatalogItem=new CatalogItem(movieItem.getName(),"Mass",movieRatings.get(i).getRatingId());
+			movieCatalogItemList.add(movieCatalogItem);
+		}
+		
+		return movieCatalogItemList;
 	}
 
 }

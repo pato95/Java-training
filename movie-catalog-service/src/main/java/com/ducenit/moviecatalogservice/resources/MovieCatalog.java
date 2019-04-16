988b082270d89b744c5947165a692ee0364033df
Viswanathan.Mahadeva <Viswanathan.Mahadeva@DUCENTRNG-14.ducenitchn.com>
2019-04-16 20:34:46 +0530

    Code with service discovery

--- movie-catalog-service/src/main/java/com/ducenit/moviecatalogservice/resources/MovieCatalog.java
+++ movie-catalog-service/src/main/java/com/ducenit/moviecatalogservice/resources/MovieCatalog.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,13 +28,13 @@ public class MovieCatalog {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId)
 	{
-		UserRatings userRatings=restTemplate.getForObject("http://localhost:8083/ratings/users/"+userId,UserRatings.class);
+		UserRatings userRatings=restTemplate.getForObject("http://movie-rating-service"+"/ratings/users/"+userId,UserRatings.class);
 		List<Rating> movieRatings=userRatings.getMovieRatings();
 		List<CatalogItem> movieCatalogItemList=new ArrayList<CatalogItem>();
 		CatalogItem movieCatalogItem;
 		for(int i=0;i<movieRatings.size();i++)
 		{
-			MovieItem movieItem=restTemplate.getForObject("http://localhost:8082/movies/"+movieRatings.get(i).getMovieId(),MovieItem.class);
+			MovieItem movieItem=restTemplate.getForObject("http://movie-info-service"+"/movies/"+movieRatings.get(i).getMovieId(),MovieItem.class);
 			movieCatalogItem=new CatalogItem(movieItem.getName(),"All Time classic ",movieRatings.get(i).getRatingId());
 			movieCatalogItemList.add(movieCatalogItem);
 		}
